b4b9f1fdba7eab9b321e67d181a1c4bb4eec503a
Jeff Lau <jeff-lau@live.com>
2020-09-16 00:42:21 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers'
 import { namehash } from './utils/namehash'
 import { abi as ensContract } from '@ensdomains/ens/build/contracts/ENS.json'
 import { abi as resolverContract } from '@ensdomains/resolver/build/contracts/Resolver.json'
@@ -6,73 +7,69 @@ let registries = {
   1: '0x314159265dd8dbb310642f98f50c066173c1259b',
   3: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
   4: '0xe7410170f87102df0055eb195163a03b7f2bff4a',
-  5: '0x112234455c3a32fd11230c42e7bccd4a84e02010'
+  5: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
 }
 
 let web3
-let ENS
-
-function getNamehash(unsanitizedName) {
-  return namehash(unsanitizedName)
-}
 
 async function getNetworkId() {
   return web3.eth.net.getId()
 }
 
-async function getResolverContract(addr) {
-  return new web3.eth.Contract(resolverContract, addr, resolverContract)
+async function getResolverContract({ address, provider }) {
+  return new ethers.Contract(address, resolverContract, provider)
 }
 
-async function getENSContract() {
-  const networkId = await getNetworkId()
-  return new web3.eth.Contract(ensContract, contracts[networkId].registry)
+async function getENSContract({ address, provider }) {
+  return new ethers.Contract(address, ensContract, provider)
 }
 
-const getENS = async ensAddress => {
-  const networkId = await getNetworkId()
-  const hasRegistry = registries[networkId] !== undefined
+class Resolver {
+  constructor(address, ENS) {
+    this.address = address
+  }
+}
 
-  if (!ENS) {
-    if (!hasRegistry && !ensAddress) {
-      throw new Error(`Unsupported network ${networkId}`)
-    } else if (!ensAddress) {
-      ensAddress = registries[networkId]
-    }
-
-    contracts[networkId].registry = ensAddress
-  } else {
-    return ENS
+class Name {
+  constructor(options) {
+    const { name, ens, provider } = options
+    this.ens = ens
+    this.name = name
+    this.namehash = namehash(name)
   }
 
-  const ENSContract = await getENSContract()
-  ENS = ENSContract
-
-  return ENSContract
-}
-
-async function resolve(input) {
-  const ENS = await getENS()
-  const isAddress = web3.utils.isAddress(input)
-  if (isAddress) {
-    const reverseName = `${input.slice(0, 2)}.addr.reverse`
-    const reverseNode = getNamehash(reverseName)
-    const resolverAddr = await ENS.resolver(reverseNode).call()
-    const Resolver = await getResolverContract(resolverAddr)
-    const name = await Resolver.name(reverseNode).call()
-
-    return name
+  getAddr() {
+    return
   }
 
-  const namehash = getNamehash(input)
-  const resolverAddr = await ENS.resolver(namehash).call()
-  const Resolver = await getResolverContract(resolverAddr)
-  return Resolver.name(namehash).call()
+  getOwner() {
+    return this.ens.owner(namehash(name))
+  }
 }
 
-export default function setupENSLite({ web3: web3Instance, ensAddress }) {
-  web3 = web3Instance
-  ensAddress = ensAddress
+export default class ENS {
+  constructor(options) {
+    const { networkId, provider, ensAddress } = options
+    this.provider = new ethers.providers.Web3Provider(provider)
+    this.ens = getENSContract({
+      address: ensAddress ? ensAddress : registries[networkId],
+      provider: this.provider,
+    })
+  }
 
-  return resolve
+  getName(name) {
+    return new Name({
+      name,
+      ens: this.ens,
+      provider: this.provider,
+    })
+  }
+  getResolver(address) {
+    return new Resolver(address)
+  }
+
+  getReverseRecord() {}
+  setReverseRecord() {}
 }
+
+export { namehash }

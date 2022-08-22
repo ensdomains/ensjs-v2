e8f788f1f724cc683089c8c936fad3554cbc5917
Hui Zhao <hui.h.zhao@icloud.com>
2022-08-22 17:29:08 +0200

    update rpc endpoints for bsc_mainnet, bsc_testnet, and eth_mainnet

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const SID = require('./dist/index').default
-const rpc =  require('./dist/constants/rpc')
+const RPC =  require('./dist/constants/rpc')
 const SIDfunctions = require('./dist/index')                                                                                                                                                                                
 const Web3 = require('web3')                                                                                                                
 
 let sid 
 
 async function main(name) {
-  const provider = new Web3.providers.HttpProvider(rpc.rpc_api)
+  const provider = new Web3.providers.HttpProvider(RPC.apis.bsc_mainnet)
   sid = new SID({ provider, sidAddress: SIDfunctions.getSidAddress('56') })
 
   const address = await sid.name(name).getAddress() // 0x123                                                                                
--- src/constants/rpc.js
+++ src/constants/rpc.js
@@ -1,1 +1,6 @@
-export const rpc_api = "https://bsc-mainnet.nodereal.io/v1/c4bc3afc2f804d4ea74fafb295828c97"
+export const apis = {
+    bsc_mainnet: "https://bsc-mainnet.nodereal.io/v1/c4bc3afc2f804d4ea74fafb295828c97",
+    bsc_testnet: "https://bsc-testnet.nodereal.io/v1/c4e97743e5674252bb0f7f7b6d4b933e",
+    eth_mainnet: "https://eth-mainnet.nodereal.io/v1/bb727c94ca474d7ea1185724fdc87a49"
+} 
+

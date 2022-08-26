91944e4ddc3cce5d8992ddc8b4ef356cf99ad9bf
yang-y888 <110803206+yang-y888@users.noreply.github.com>
2022-08-26 17:20:36 +0800

    Fix validate err (#11)
    * fix: validate err
    
    * v0.1.10

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,49 +1,49 @@
-import { ethers } from 'ethers'
-import { validate as ensValidate } from '@ensdomains/ens-validation'
-import { toArray } from 'lodash'
+import {ethers} from 'ethers'
+import {validate as ensValidate} from '@ensdomains/ens-validation'
+import {toArray} from 'lodash'
 import {
-  isEncodedLabelhash,
-  isDecrypted,
-  decodeLabelhash,
-  encodeLabelhash,
-  labelhash,
+    isEncodedLabelhash,
+    isDecrypted,
+    decodeLabelhash,
+    encodeLabelhash,
+    labelhash,
 } from './labelhash'
 import {
-  encodeContenthash,
-  decodeContenthash,
-  isValidContenthash,
+    encodeContenthash,
+    decodeContenthash,
+    isValidContenthash,
 } from './contents'
-import { normalize } from 'eth-ens-namehash'
-import { namehash } from './namehash'
+import {normalize} from 'eth-ens-namehash'
+import {namehash} from './namehash'
 
 //import { checkLabelHash } from '../updaters/preImageDB'
 
 const uniq = (a, param) =>
-  a.filter(
-    (item, pos) =>
-      a.map((mapItem) => mapItem[param]).indexOf(item[param]) === pos
-  )
+    a.filter(
+        (item, pos) =>
+            a.map((mapItem) => mapItem[param]).indexOf(item[param]) === pos
+    )
 
 const checkLabels = (...labelHashes) => labelHashes.map((hash) => null)
 
 function getEnsStartBlock(networkId) {
-  switch (networkId) {
-    case 1:
-    case '1':
-      return 3327417
-    case 3:
-    case '3':
-      return 25409
-    default:
-      return 0
-  }
+    switch (networkId) {
+        case 1:
+        case '1':
+            return 3327417
+        case 3:
+        case '3':
+            return 25409
+        default:
+            return 0
+    }
 }
 
 // export const checkLabels = (...labelHashes) =>
 //   labelHashes.map(labelHash => checkLabelHash(labelHash) || null)
 
 const mergeLabels = (labels1, labels2) =>
-  labels1.map((label, index) => (label ? label : labels2[index]))
+    labels1.map((label, index) => (label ? label : labels2[index]))
 
 function validateLabelLength(name) {
     if (!name) {
@@ -64,7 +64,7 @@ function validateLabelLength(name) {
     return true
 }
 
-function validateDomains(value){
+function validateDomains(value) {
     const nospecial = /^[^*|\\":<>[\]{}`\\\\()';@&$]+$/u
     const blackList =
         // eslint-disable-next-line no-control-regex
@@ -80,7 +80,7 @@ function validateDomains(value){
 }
 
 function validateName(name) {
-    if (!name){
+    if (!name) {
         throw new Error('Invalid name');
     }
     let domain = name;
@@ -88,89 +88,92 @@ function validateName(name) {
     let i = name.lastIndexOf('.');
     if (i > 0) {
         domain = name.substring(0, i);
-        suffix = name.substring(i);
+        suffix = name.substring(i + 1);
     }
-  const nameArray = [domain, suffix]
-  const hasEmptyLabels = nameArray.filter((e) => e.length < 1).length > 0
-  if (hasEmptyLabels) throw new Error('Domain cannot have empty labels');
-  if (!validateLabelLength(domain)) throw new Error('Invalid name');
-  if (!validateDomains(domain)) throw new Error('Invalid name');
-  const normalizedArray = nameArray.map((label) => {
-    return isEncodedLabelhash(label) ? label : normalize(label)
-  })
-  try {
-    return normalizedArray.join('.')
-  } catch (e) {
-    throw e
-  }
+    const nameArray = [domain];
+    if (i > 0) {
+        nameArray.push(suffix)
+    }
+    const hasEmptyLabels = nameArray.filter((e) => e.length < 1).length > 0
+    if (hasEmptyLabels) throw new Error('Domain cannot have empty labels');
+    if (!validateLabelLength(domain)) throw new Error('Invalid name');
+    if (!validateDomains(domain)) throw new Error('Invalid name');
+    const normalizedArray = nameArray.map((label) => {
+        return isEncodedLabelhash(label) ? label : normalize(label)
+    })
+    try {
+        return normalizedArray.join('.')
+    } catch (e) {
+        throw e
+    }
 }
 
 function isLabelValid(name) {
-  try {
-    validateName(name)
-    if (name.indexOf('.') === -1) {
-      return true
+    try {
+        validateName(name)
+        if (name.indexOf('.') === -1) {
+            return true
+        }
+    } catch (e) {
+        console.log(e)
+        return false
     }
-  } catch (e) {
-    console.log(e)
-    return false
-  }
 }
 
 const parseSearchTerm = (term, validTld) => {
-  let regex = /[^.]+$/
+    let regex = /[^.]+$/
 
-  try {
-    validateName(term)
-  } catch (e) {
-    return 'invalid'
-  }
-
-  if (term.indexOf('.') !== -1) {
-    const termArray = term.split('.')
-    const tld = term.match(regex) ? term.match(regex)[0] : ''
-    if (validTld) {
-      if (tld === 'bnb' && termArray[termArray.length - 2].length < 3) {
-        return 'short'
-      }
-      return 'supported'
+    try {
+        validateName(term)
+    } catch (e) {
+        return 'invalid'
     }
 
-    return 'unsupported'
-  } else if (ethers.utils.isAddress(term)) {
-    return 'address'
-  } else {
-    //check if the search term is actually a tld
-    if (validTld) {
-      return 'tld'
+    if (term.indexOf('.') !== -1) {
+        const termArray = term.split('.')
+        const tld = term.match(regex) ? term.match(regex)[0] : ''
+        if (validTld) {
+            if (tld === 'bnb' && termArray[termArray.length - 2].length < 3) {
+                return 'short'
+            }
+            return 'supported'
+        }
+
+        return 'unsupported'
+    } else if (ethers.utils.isAddress(term)) {
+        return 'address'
+    } else {
+        //check if the search term is actually a tld
+        if (validTld) {
+            return 'tld'
+        }
+        return 'search'
     }
-    return 'search'
-  }
 }
 
 const emptyAddress = '0x0000000000000000000000000000000000000000'
 
 export {
-  // general utils
-  uniq,
-  emptyAddress,
-  getEnsStartBlock,
-  checkLabels,
-  mergeLabels,
-  // name validation
-  validateName,
-  parseSearchTerm,
-  isLabelValid,
-  // labelhash utils
-  labelhash,
-  isEncodedLabelhash,
-  isDecrypted,
-  decodeLabelhash,
-  encodeLabelhash,
-  // namehash utils
-  namehash,
-  // contents utils
-  encodeContenthash,
-  decodeContenthash,
-  isValidContenthash,
+    // general utils
+    uniq,
+    emptyAddress,
+    getEnsStartBlock,
+    checkLabels,
+    mergeLabels,
+    // name validation
+    validateName,
+    parseSearchTerm,
+    isLabelValid,
+    // labelhash utils
+    labelhash,
+    isEncodedLabelhash,
+    isDecrypted,
+    decodeLabelhash,
+    encodeLabelhash,
+    // namehash utils
+    namehash,
+    // contents utils
+    encodeContenthash,
+    decodeContenthash,
+    isValidContenthash,
 }

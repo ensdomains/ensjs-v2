4bf4c9eeb0456dafc2ec3e3fe4c174ac511daca0
sheep331 <sheep@arb.id>
2023-01-11 12:30:32 +0800

    feat: add address

--- src/index.js
+++ src/index.js
@@ -14,12 +14,15 @@ import {
 const utils = ethers.utils
 
 function getSidAddress(networkId) {
-  if ([97].includes(parseInt(networkId))) {
+  const id = parseInt(networkId);
+  if ([97].includes(id)) {
     return '0xfFB52185b56603e0fd71De9de4F6f902f05EEA23'
-  } else if ([1, 3, 4, 5].includes(parseInt(networkId))) {
+  } else if ([1, 3, 4, 5].includes(id)) {
     return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
-  } else if ([56].includes(parseInt(networkId))) {
+  } else if ([56].includes(id)) {
     return '0x08CEd32a7f3eeC915Ba84415e9C07a7286977956'
+  } else if ([421613].includes(id)) {
+    return '0x1f70fc8de5669eaa8C9ce72257c94500DC5ff2E4'
   }
 }
 
